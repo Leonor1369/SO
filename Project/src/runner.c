686e519dc8703907014820e9b6a325f424dc9a8c
leonor36 <a109820@uminho.pt>
2026-04-25 17:54:06 +0100

    tratar cada mensagem(por acabar), autorizar execuçoes, log persistente em ficheiro

--- Project/src/runner.c
+++ Project/src/runner.c
@@ -68,57 +68,115 @@ void handle_execute(int argc, char *argv[]) {
 
     pid_t my_pid = getpid();
     char runner_fifo[64];
-    get_runner_fifo(runner_fifo, my_pid);
+    get_runner_fifo(runner_fifo, sizeof(runner_fifo), my_pid);
 
     // 1. criar o FIFO de resposta
     mkfifo(runner_fifo, 0666);
-    write(1, "FIFO criado\n", 12);
     
     // 2. abrir o FIFO do controller para escrita
-    int fd_ctrl = open(CONTROLLER_FIFO, O_WRONLY);
+    Message msg = {0};
+    msg.type = MSG_EXECUTE;
+    msg.runner_pid = my_pid;
+    strncpy(msg.user_id, argv[2], MAX_USER_LEN);
+    strncpy(msg.command, agrv[3], MAX_CMD_LEN);
 
-    // 3. preencher a mensagem e enviar
-    Message msg = { .type = MSG_EXECUTE, .runner_pid = my_pid, .user_id = atoi(argv[2]) };
+    int fd_ctrl = open(CONTROLLER_FIFO, O_WRONLY);
     write(fd_ctrl, &msg, sizeof(msg));
     close(fd_ctrl);
 
-    // 4. notificar utilizador
-    write(STDOUT_FILENO, "[runner] command X submitted\n", 30);
+    // 3. notificar utilizador
+    write(STDOUT_FILENO, "[runner] command submited\n", 28);
 
-    // 5. abrir o FIFO de resposta e aguardar autorização
+    // 4. aguardar autorizaçao
     int fd_resp = open(runner_fifo, O_RDONLY);
     Message auth;
     read(fd_resp, &auth, sizeof(auth));
+    close(fd_resp);
 
-    // 6. notificar utilizador que vai executar
-    write(STDOUT_FILENO, "[runner] executing command X...\n", 33);
-
-    // 7. fork + exec
+    // 5. executar o commando
+    write(STDOUT_FILENO, "[runner] executing...\n", 22);
     pid_t child = fork();
-    // if (child == 0) { execvp(...); }
-    // else { waitpid(child, NULL, 0); }
+    if (child == 0) {
+        // filho: executar commando
+        char *args[] = {"/bin/sh", -c, argv[3], NULL};
+        execvp(args[0], args);
+        _exit(1);
+    }
+    int status;
+    waitpid(child, &status, 0);
 
-    // 8. notificar utilizador que terminou
-    // write(STDOUT_FILENO, "[runner] command X finished\n", ...);
-
-    // 9. enviar MSG_DONE ao controller
+    // 6.calcular duraçao e enviar MSG_DONE
     // ...
 
-    // 10. limpar o FIFO de resposta
-    // unlink(runner_fifo);
+    // 7. limpar o FIFO de resposta
+    unlink(runner_fifo);
 }
 
 
 
-void handle_query() {
+void handle_query(void) {
     // semelhante ao execute mas envia MSG_QUERY
     // e imprime a resposta no stdout
+
+    pid_t my_pid = getpid();
+    char runner_fifo[64];
+    get_runner_fifo(runner_fifo, sizeof(runner_fifo), my_pid);
+
+    // 1. criar o FIFO de resposta
+    mkfifo(runner_fifo, 0666);
+    
+    // 2. enviar pedido
+    Message msg = {.type = MSG_QUERY, .runner_pid = my_pid};
+
+    int fd_ctrl = open(CONTROLLER_FIFO, O_WRONLY);
+    write(fd_ctrl, &msg, sizeof(msg));
+    close(fd_ctrl);
+
+    // 3. receber e imprimir resposta
+    // o controller envia uma string com a lista
+    int fd_resp = open(runner_fifo, O_RDONLY);
+
+    char buf[4096];
+    ssize_t n = read(fd_resp, buf, sizeof(buf)-1);
+    buf[n]= '\n';
+    write(STDOUT_FILENO, buf, n);
+    close(fd_resp);
+
+    unlink(runner_fifo);
+
 }
 
 
 void handle_shutdown() {
     // envia MSG_SHUTDOWN
-    // aguarda confirmação do controller
+    // aguarda confirmação MSG_SHUTDOWN_OK do controller
+
+    pid_t my_pid = getpid();
+    char runner_fifo[64];
+    get_runner_fifo(runner_fifo, sizeof(runner_fifo), my_pid);
+
+    // 1. criar o FIFO de resposta
+    mkfifo(runner_fifo, 0666);
+    
+    // 2. enviar pedido
+    Message msg = {.type = MSG_SHUTDOWN, .runner_pid = my_pid};
+
+    int fd_ctrl = open(CONTROLLER_FIFO, O_WRONLY);
+    write(fd_ctrl, &msg, sizeof(msg));
+    close(fd_ctrl);
+
+    // 3. receber confirmação do controller
+    int fd_resp = open(runner_fifo, O_RDONLY);
+    Message resp;
+    ssize_t n = read(fd_resp, &resp, sizeof(resp));
+    if (n == sizeof(resp) && resp.type == MSG_SHUTDOWN_OK) {
+        out("[runner] shutdown confirmado pelo controller\n");
+    } else {
+        out("[runner] erro ao receber confirmação de shutdown\n");
+    }
+    close(fd_resp);
+
+    unlink(runner_fifo);
 }
 
 int main(int argc, char *argv[]) {
